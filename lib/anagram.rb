653d09b34ae89fa273a75fcdd91ece7f69864d44
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-01 18:10:36 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,7 @@ class Anagram
   end
 
   def match(words)
-    words.each {|matching_word| return matching_word if matching_word.chars.sort_by(&:downcase).join == word.chars.sort_by(&:downcase).join  }
+    words.select {|matching_word| matching_word.chars.sort_by(&:downcase).join == word.chars.sort_by(&:downcase).join  }
   end
 
 end
